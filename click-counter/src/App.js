647a67ead1ad5ae75bc74013631f8f087e905961
Galina Gryaznova <galina.gryaznova@scotts.com>
2020-08-17 13:37:52 +0300

    Ejection provided

---
+++ click-counter/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+
+        return (
+
+            <div data-test='component-app'>
+                App
+            </div>
+        )
+
+    }
+}
+
+export default App;
---
+++ click-counter/src/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+import App from './App';
+
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+test('renders without errors', () => {
+    const wrapper = shallow(<App/>)
+    const appComponent = wrapper.find("[data-test='component-app']")
+    expect(appComponent.length).toBe(1)
+});
+test('renders increment button', () => {
+
+});
+test('renders counter display', () => {
+
+});
+test('counter starts at 0', () => {
+
+});
+test('clicking a button increments counter display', () => {
+
+});
+
