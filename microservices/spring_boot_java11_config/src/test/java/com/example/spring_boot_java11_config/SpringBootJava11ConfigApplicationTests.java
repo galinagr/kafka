c17384e3f7f687e45cef853e18fc8e61bde7fc29
Galina Gryaznova <galina.gryaznova@scotts.com>
2021-04-23 14:07:13 +0300

    add

---
+++ microservices/spring_boot_java11_config/src/test/java/com/example/spring_boot_java11_config/SpringBootJava11ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_java11_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJava11ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
